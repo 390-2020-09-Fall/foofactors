12d7683060acbfe8afc368f81fbc7d9b55a38873
rudeboybert <albert.ys.kim@gmail.com>
2020-09-23 15:46:17 -0400

    Adding toy data

---
+++ data-raw/DATASET.R
@@ -0,0 +1,8 @@
+## code to prepare `DATASET` dataset goes here
+library(tidyverse)
+
+toy_data <- tibble(
+  name = c("Guy Incognito", "Cosme Fulanito", "Albert Y. Kim"),
+  number = c(9, 79, 76)
+)
+usethis::use_data(toy_data, overwrite = TRUE)
