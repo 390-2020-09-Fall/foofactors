e5421cc563392be0c52dc899ff898f4f378298d4
oliviahandoko <ohandoko@smith.edu>
2020-09-29 19:55:28 -0400

    simply multiply function

---
+++ R/fmultiply.R
@@ -0,0 +1,15 @@
+#' Multiply two integers
+#'
+#' Create product of two existing random integers, where the new integer is the multiplication
+#' of the two random input integers.
+#'
+#' @param x integer
+#' @param y integer
+#'
+#' @return product
+#' @export
+#' @examples
+#' fmultiply(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
+fmultiply <- function(x,y) {
+  x*y
+}
