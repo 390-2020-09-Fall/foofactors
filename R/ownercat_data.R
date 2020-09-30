0c854bf9ee62a5538d84dfdfe1bccb85ee7eaf89
oliviahandoko <ohandoko@smith.edu>
2020-09-29 21:34:38 -0400

    cats dataset

---
+++ R/ownercat_data.R
@@ -0,0 +1,14 @@
+#' Owner Cat Data
+#'
+#' Based on R Packages Chapter 14
+#'
+#' @format A data frame with 3 rows and 2 variables:
+#' \describe{
+#'   \item{name}{Owner's Name}
+#'   \item{number}{Cat's Name}
+#'   ...
+#' }
+#' @source \url{https://r-pkgs.org/data.html#data-dataraw/}
+"ownercat_data"
+
+
---
+++ data-raw/DATASET2.R
@@ -0,0 +1,8 @@
+## code to prepare `DATASET` dataset goes here
+library(tidyverse)
+
+ownercat_data <- tibble(
+  Owner = c("Bob", "Sarah", "Olivia"),
+  catname = c("Mooncake", "Cheese", "Oreo")
+)
+usethis::use_data(ownercat_data, overwrite = TRUE)
