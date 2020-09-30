0508a5761c38468d486a429898e652b2bb206d04
Starryz <zhouyujiaa@gmail.com>
2020-09-30 12:26:53 -0400

    changed author, document function

--- R/oz-to-kg.R
+++ R/oz-to-kg.R
@@ -1,3 +1,12 @@
+#' OZ-KG unit conversion function
+#'
+#' @param weight in oz
+#'
+#' @return weight in kg
+#' @export
+#'
+#' @examples
+#' oz_to_kg(1)
 oz_to_kg <- function(weight_oz) {
   weight_kg <- weight_oz/35.274
   return(weight_kg)
