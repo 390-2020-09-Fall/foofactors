e31d414bbaf3838e64168a51229446ebce0cc293
Starryz <zhouyujiaa@gmail.com>
2020-09-30 12:34:11 -0400

    change file name

--- R/oztokg.R
+++ R/oztokg.R
@@ -6,8 +6,8 @@
 #' @export
 #'
 #' @examples
-#' oz_to_kg(1)
-oz_to_kg <- function(weight_oz) {
+#' oztokg(1)
+oztokg <- function(weight_oz) {
   weight_kg <- weight_oz/35.274
   return(weight_kg)
 }
