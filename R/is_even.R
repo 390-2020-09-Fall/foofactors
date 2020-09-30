c574e58c38b950d1ab4b0efe425043a92c34bd67
unknown <limisoph@gmail.com>
2020-09-30 01:08:57 -0700

    adding documentation

--- R/is_even.R
+++ R/is_even.R
@@ -1,3 +1,13 @@
+#' Check if number(s) is/are even
+#'
+#' @param nums numeric
+#'
+#' @return logical
+#' @export
+#'
+#' @examples
+#' is_even(3)
+#' is_even(c(4,5))
  is_even <- function(nums) {
   results <- nums %% 2 == 0
   return(results)
