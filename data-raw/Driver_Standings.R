4f5209cda4341e987ff03a8df3a2a8a4ea9d3110
Serena Li <sli58@smith.edu>
2020-09-30 18:44:28 +0800

    Added dataset

---
+++ data-raw/Driver_Standings.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+
+F1_Drivers_Standings <- tibble(
+  Driver = c("Lewis_Hamilton", "Valtteri_Bottas", "Max_Verstappen"),
+  PTS = c(205, 161, 128)
+)
+
+usethis::use_data(F1_Drivers_Standings, overwrite = TRUE)
