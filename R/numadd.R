070f7fd34b6bdbeaf2b92e108581fa956b4ebb5d
Haley Schmidt <Haley@Haleys-Air.T-mobile.com>
2020-10-02 13:44:20 -0500

    adds 2 numbers

---
+++ R/numadd.R
@@ -0,0 +1,15 @@
+#' Add two numbers
+#'
+#' Add two numbers together.
+#'
+#' @param a number
+#' @param b number
+#'
+#' @return number
+#' @export
+#' @examples
+#' numadd(10, 12)
+numadd <- function(a, b) {
+  result <- (a + b)
+  return(result)
+}
