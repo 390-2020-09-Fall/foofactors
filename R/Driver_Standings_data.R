4160c20730ae8343bb4e228d1a1402e01060faf1
Serena Li <sli58@smith.edu>
2020-10-08 00:51:02 +0800

    changed the dataset name try to have a .rda file

---
+++ R/Driver_Standings_data.R
@@ -0,0 +1,12 @@
+#' Driver_Standings Data
+#'
+#' Based on R Packages Chapter 14
+#'
+#' @format A data frame with 2 rows and 2 variables:
+#' \describe{
+#'   \item{Driver}{Driver's Name}
+#'   \item{PTS}{Points}
+#'   ...
+#' }
+#' @source \url{https://r-pkgs.org/data.html#data-data/}
+"Driver_Standings_data"
--- data-raw/Driver_Standings.R
+++ data-raw/Driver_Standings.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 
-F1_Drivers_Standings <- tibble(
+Driver_Standings <- tibble(
   Driver = c("Lewis_Hamilton", "Valtteri_Bottas", "Max_Verstappen"),
   PTS = c(205, 161, 128)
-
 )
 
-usethis::use_data(F1_Drivers_Standings, overwrite = TRUE)
+usethis::use_data(Driver_Standings, overwrite = TRUE)
+
