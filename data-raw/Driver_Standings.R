a8af9dac2e5862d2db67d2f2a0b9d26fe42134d2
Serena Li <sli58@smith.edu>
2020-10-08 01:00:16 +0800

    changed dataset name

--- data-raw/Driver_Standings.R
+++ data-raw/Driver_Standings.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 
-Driver_Standings <- tibble(
+Driver_Standings_data <- tibble(
   Driver = c("Lewis_Hamilton", "Valtteri_Bottas", "Max_Verstappen"),
   PTS = c(205, 161, 128)
 )
 
-usethis::use_data(Driver_Standings, overwrite = TRUE)
+usethis::use_data(Driver_Standings_data, overwrite = TRUE)
 
