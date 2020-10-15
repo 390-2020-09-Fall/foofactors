0b427680503434e5b7ee114bc899b6d9c069c3e1
Emma King <eking03@smith.edu>
2020-10-15 13:50:38 -0400

    Emma adding function; Not yet sure how to make the function description show up in help

---
+++ R/stem_code_filter.R
@@ -0,0 +1,77 @@
+#' Filters ForestGeo's SCBI tree census data by stem code. Allows user to select for or exclude entries based on the presence of user-defined stem code(s).
+#' Please reference the following website for stem code definitions: https://github.com/SCBI-ForestGEO/SCBI-ForestGEO-Data/blob/master/tree_main_census/metadata/SCBI_stem_codes.pdf.
+#'
+#' @param df data frame
+#' @param stem_code character or list
+#' @param include logical
+#'
+#' @return data frame
+#' @export
+#'
+#' @examples
+#' treecensus_example <- read.csv(text=getURL("https://raw.githubusercontent.com/390-2020-09-Fall/SCBI-ForestGEO-Data/master/tree_main_census/data/census-csv-files/scbi.stem3.csv"))
+#' my_codes <- c("A", "M", "DC", "D", "B")
+#' stem_code_filter(treecensus_example, my_codes)
+#'
+
+stem_code_filter <- function(df, stem_code, include = TRUE) {
+
+
+  df <- df %>% mutate(codes = ifelse(codes == "NULL", NA, codes))
+
+
+  # Renaming codes that have two or more characters
+
+  df <- df %>% mutate(codefind = codes)
+
+  twochr <- c("DC", "DN", "DS", "DT", "TR", "WR", "lost")
+  twochr_new <- c(1:7)
+
+  a <- 1
+
+  for (i in twochr) {
+
+    df <- df %>%
+      mutate(codefind = ifelse(grepl(i, codefind), gsub(i, twochr_new[a], codefind), codefind))
+
+    for (j in stem_code) {
+
+      twochr_logical <- ifelse(grepl(i, j), 1, 0)
+
+      if (twochr_logical > 0) {
+        stem_code <- gsub(i, twochr_new[a], stem_code)
+      }
+
+      else{stem_code}
+    }
+
+    a <- a + 1
+  }
+
+
+  # Searching for user selected codes
+
+  df <- df %>% mutate(codeselect = "ignore")
+
+  for (j in stem_code) {
+    df <- df %>%
+      mutate(codeselect = ifelse(grepl(j, codefind), "select", codeselect))
+  }
+
+
+  # Removing added columns and filtering
+  ## default 'include = TRUE': filter to only include rows with selected codes
+  ## alternative 'include = FALSE': filter to exclude rows with selected codes
+
+  if (include == TRUE) {
+    df <- df %>% filter(codeselect == "select")
+  }
+
+  else {
+    df <- df %>% filter(codeselect != "select")
+  }
+
+  df %>%
+    dplyr::select(-codefind, -codeselect)
+
+}
