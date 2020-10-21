d01e3c713fbd4a43eb187fabbb6f6fd34f84ac8c
Serena Li <sli58@smith.edu>
2020-10-21 13:06:10 +0800

    deleted drvier standings

--- data-raw/Driver_Standings.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-
-Driver_Standings_data <- tibble(
-  Driver = c("Lewis_Hamilton", "Valtteri_Bottas", "Max_Verstappen"),
-  PTS = c(205, 161, 128)
-)
-usethis::use_data(Driver_Standings_data, overwrite = TRUE)
-
-
