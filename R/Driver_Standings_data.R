16c85ffc733d409f958e5d6e4d9caa71755d2df2
Serena Li <sli58@smith.edu>
2020-10-21 13:03:42 +0800

    changed test-Add.R in testthat

--- R/Driver_Standings_data.R
+++ R/Driver_Standings_data.R
@@ -2,7 +2,7 @@
 #'
 #' Based on R Packages Chapter 14
 #'
-#' @format A data frame with 2 rows and 2 variables:
+#' @format A data frame with 3 rows and 2 variables:
 #' \describe{
 #'   \item{Driver}{Driver's Name}
 #'   \item{PTS}{Points}
