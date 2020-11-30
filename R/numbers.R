620f132130ebe1773d3d312a0874875f03d90d96
Haley Schmidt <Haley@Haleys-Air.lan>
2020-11-29 19:14:06 -0600

    moved dataset to correct folder

---
+++ R/numbers.R
@@ -0,0 +1,11 @@
+#' numbers data
+#'
+#' Based on R Packages Chapter 14
+#'
+#' @format A data frame with 10 rows and 1 variables:
+#' \describe{
+#'   \item{number}{Number}
+#'   ...
+#' }
+#' @source \url{https://r-pkgs.org/data.html#data-data/}
+"numbers"
