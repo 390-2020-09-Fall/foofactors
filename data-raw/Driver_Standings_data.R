401fc453dc261221c7e6040d3abf7237af661af1
Serena Li <sli58@smith.edu>
2020-12-02 11:38:50 +0800

    Initial commit

--- data-raw/Driver_Standings_data.R
+++ data-raw/Driver_Standings_data.R
@@ -4,5 +4,5 @@ Driver_Standings_data <- tibble(
   Driver = c("Lewis_Hamilton", "Valtteri_Bottas", "Max_Verstappen"),
   PTS = c(205, 161, 128)
 )
-usethis::use_data(Driver_Standings_data, overwrite = TRUE)
+usethis::use_data_raw(Driver_Standings_data, overwrite = TRUE)
 
